e18ab090832d24b1fd42a73fe7847e933bebcaa2
Roan Bascoe <roanbascoe5@gmail.com>
2020-07-08 19:28:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.map do |key, value, array|
+  data.map do |(key, value), array|
     value.map do |inner_key, names|
       names.map do |name|
         if !array[name]
-        array[name] = {}
+          array[name] = {}
         end
         if !array[name][key]
-        !array[name][key] = []
+          !array[name][key] = []
         end
         array[name][key].push(inner_key.to_s)
       end
