0250d7cc57ebdb9077c037e78503cc2e42ea3155
Roan Bascoe <roanbascoe5@gmail.com>
2020-07-08 19:23:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.map{|key, value|
-  if value.class == String
-    value = key
+  data.map{|key, value, array|
+    value.map { |inner_key, names|
+      names.map{|name|
+        if !array[name]{
+        array[name] = {}
+        }
+        if !array[name][key]{
+        !array[name][key] = []
+        }
+        array[name][key].push(inner_key.to_s)
+      }
+      
+    }
+    
   }
 end
